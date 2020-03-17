47c4cf69b99d579ae3cfabc3a1f1aa15caf96dac
EduardoFreit <lepf@cin.ufpe.br>
2020-03-17 16:36:37 -0300

    Upadate R scripts

---
+++ data/funcR/4_team1VSteam2.R
@@ -0,0 +1,26 @@
+library(dplyr)
+
+#time1 <- toupper("santa cruz")
+#time2 <- toupper("flamengo")
+
+time1VStime2 <- function(time1, time2){ 
+  tableTime <- readRDS("data/tables/TabelaDeTimesBR.rds")
+  
+  idTime1 <- tableTime %>% 
+    select(NomeTime, ID) %>% 
+    filter(NomeTime == toupper(time1)) %>% 
+    select(ID) %>% 
+    as.numeric()
+  
+  idTime2 <- tableTime %>% 
+    select(NomeTime, ID) %>% 
+    filter(NomeTime == toupper(time2)) %>% 
+    select(ID) %>% 
+    as.numeric()
+  
+  paste("https://www.ogol.com.br/team_adversario.php?id=",
+        idTime1,
+        "&fk_adv=",
+        idTime2, 
+        sep = "")
+}
