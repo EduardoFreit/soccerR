7c3466c20b80c5ff341722ff27809d60e6de1b5b
EduardoFreit <lepf@cin.ufpe.br>
2020-03-17 16:36:37 -0300

    Upadate R scripts

---
+++ data/funcR/3_crwlTeamSoccBR.R
@@ -0,0 +1,73 @@
+library(rvest)
+library(xml2)
+library(dplyr)
+library(stringr)
+aux <- "https://www.ogol.com.br/search_team.php?search_string=&sta=&nac=6&posi=&peq=&ida=&cap=&ord=i&op=all&page=1"
+vs <- "https://www.ogol.com.br/team_adversario.php?id=2244&fk_adv=2240"
+
+teamsDestq <- function(link){
+  Sys.sleep(runif(1, 5.3, 7.8))
+  teamsHTML1 <- link %>% 
+    read_html() %>% 
+    html_nodes('.zztable') %>% 
+    .[1] %>% html_nodes("div a")
+  
+  teamsNM1 <- teamsHTML1 %>%
+    html_text() %>% 
+    toupper()
+  
+  teamsID1 <- teamsHTML1 %>% 
+    html_attr("href") %>% 
+    .[nchar(teamsID1) <= 23] %>% 
+    gsub(".*=", "", .) %>% as.numeric()
+  
+  teamDestq <- c(teamsNM1,teamsID1)
+}
+
+crawlerTeam <- function(link){
+  
+  teamsHTML<- link %>% read_html() %>%  
+    html_nodes(".zztable.stats tr div div a")
+  
+  teamsNM <- teamsHTML %>% 
+    html_nodes("b") %>% 
+    html_text() %>% 
+    toupper()
+  
+  teamsID <- teamsHTML %>% 
+    html_attr("href")
+  teamsID <- teamsID[nchar(teamsID) <= 23]
+  teamsID <- gsub(".*=", "", teamsID) %>% 
+    as.numeric()
+  
+  if (str_sub(link, -2 ,-1) == "=1"){
+    cat("...")
+    teamDest_aux <- teamDestq
+    teamsNM <- c(teamsNM, teamDest_aux[1])
+    teamsID <- c(teamsID, (teamDest_aux[2] %>% as.numeric()))
+  }
+  
+  data.frame(NomeTime = teamsNM, ID = teamsID)
+}
+
+tabelaTeams <- function(link){
+  source("data/funcR/2_tbLinkBrTeam.R")
+  x <- 0
+  tbLinkBrTeam(link)
+  Sys.sleep(runif(1, 3.3, 7.8))
+  linksTeams <- readRDS("data/tables/teamLinkTable.rds")
+  tbTeams <- data.frame() # tabela em branco
+  
+  for(i in linksTeams){
+    x <- x %>% 
+      sum((100/length(linksTeams))) %>% 
+      round(2)
+    
+    cat("Loading...", x,'%\n')
+    tbTeams <- tbTeams %>%
+      rbind(crawlerTeam(i))
+      Sys.sleep(runif(1, 3.3, 7.8))
+  }
+  saveRDS(tbTeams, "data/tables/TabelaDeTimesBR.rds")
+  cat("Done 100",'%\n')
+}
